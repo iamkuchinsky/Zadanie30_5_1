b4ff926e8d478bcebe51f71ab21c98b861b9ffac
iamkuchinsky <iamkuchinsky@gmail.com>
2024-05-01 22:51:31 +0500

    edit code

--- tests/test_show_all_pets.py
+++ tests/test_show_all_pets.py
@@ -1,9 +1,7 @@
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as WDW
 from selenium.webdriver.support import expected_conditions as EC
 from main import driver
-
 
 
 def test_show_all_pets(driver):
@@ -15,13 +13,12 @@ def test_show_all_pets(driver):
     driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
     # Проверяем, что мы оказались на главной странице пользователя
     assert WDW(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'h1'))).text == "PetFriends"
-    time.sleep(3)
     # Переходим во вкладку "Мои питомцы"
     driver.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[1]/a').click()
 
-    images = WDW(driver, 10).until(EC.presence_of_all_elements_located(By.CSS_SELECTOR, '.card-deck .card-img-top'))
-    names = WDW(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.card-deck .card-title')))
-    descriptions = WDW(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.card-deck .card-text')))
+    images = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-img-top')
+    names = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-title')
+    descriptions = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-text')
 
     for i in range(len(names)):
         assert images[i].get_attribute('src') != ''
@@ -31,3 +28,6 @@ def test_show_all_pets(driver):
         parts = descriptions[i].text.split(", ")
         assert len(parts[0]) > 0
         assert len(parts[1]) > 0
+
+
+
